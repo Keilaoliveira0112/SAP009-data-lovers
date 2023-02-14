f19ce58418f084d36137aede937da51f01e7d36b
Keila <keilaoliveira.copy@gmail.comm>
2023-02-13 22:29:59 -0300

    import dos cards para o JS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
-//import { example } from './data.js';
+import { } from './data.js';
+import data from './data/pokemon/pokemon.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+const arrayPokemon = data.pokemon;
 
 //console.log(example, data);
+
+
+window.addEventListener("load", () => printCards(arrayPokemon))
+
+//CHAMANDO A FUNÇÃO PARA QUANDO CARREGAR TODA A PÁGINA
+
+function printCards(array) {
+    document.querySelector(".cards").innerHTML = array.map((key) =>
+
+        `
+        <a href="carrossel.html?carrossel=${key.num}">
+            <div class="cardPokemon">
+                <p class="registro" >${key.num}</p>
+                <h1>${key.name}</h1>
+                <img class="imgCard" src="${key.img}" alt="${key.name}">
+                <p class="tipo" >${key.type.join(" ")}</p>
+            </div>
+        </a>
+    `
+    ).join("")
+
+}
+
+//FUNÇÃO PARA VIRAR O CARD
+
+
