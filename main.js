58595a8cf5d9cdde3223cc64843fdcf586f25ff7
Keila <keilaoliveira.copy@gmail.comm>
2023-03-08 17:24:34 -0300

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 import { searchData, filterPokemon, orderPokemon, calculatePercentage } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
+document.addEventListener("DOMContentLoaded", () =>{
+  alert("Sejam Bem-vindas e Bem-vindos\n\nNesse site você irá encontrar informações relevantes sobre os pokémons da região Kanto e Johto!");
+})
+
+const btnMenu = document.querySelector(".menu-toggle")
+const menu = document.querySelector(".menu-section")
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.toggle("show")
+})
+
 const resultPokemon = data.pokemon;
 
 const calculationText = document.getElementById('calculation')
@@ -52,14 +63,6 @@ function printCards(array) {
   }).join("")
 }
 printCards(resultPokemon)
-
-const btnMenu = document.querySelector(".menu-toggle")
-const menu = document.querySelector(".menu-section")
-
-btnMenu.addEventListener("click", () => {
-  menu.classList.toggle("show")
-
-})
 
 const searchInput = document.getElementById('search')
 
